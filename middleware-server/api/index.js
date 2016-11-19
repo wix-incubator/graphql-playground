2ac6b63a72d649be0ea8093f360138c62cb7e913
Martynas Bardauskas <martynasb@wix.com>
2016-11-19 14:48:00 +0200

    combined characters with films

--- middleware-server/api/index.js
+++ middleware-server/api/index.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const router = express.Router();
 import dataLoaders from '../services/dataLoaders';
-import { each, map, find } from 'lodash';
+import { each, map, flatten, find } from 'lodash';
+
+const DEFAULT_CHAR_COUNT = 3;
 
 router.get('/allFilms', function (req, res) {
+  const charCount = req.query.charCount || DEFAULT_CHAR_COUNT;
 
-  getFilmList(dataLoaders)
+  getFilmList(dataLoaders, charCount)
    .then(films => res.json(films))
    .catch(e => res.json(e));
 });
 
 export default router;
 
-function getFilmList(loaders) {
+function getFilmList(loaders, charCount) {
   return loaders.film.loadAll()
-    .then(films => includeCharactersToFilms(loaders, films))
+    .then(films => includeCharactersToFilms(loaders, films, charCount))
     ;
 }
 
-function includeCharactersToFilms(loaders, films) {
-  return films;
+function includeCharactersToFilms(loaders, films, charCount) {
+  const charsToGet = map(films, film => film.characters.slice(0, charCount));
+  const charsPromises = map(charsToGet, charUrls => loaders.film.loadByUrl(charUrls));
+
+  return Promise.all(charsPromises)
+    .then(resolvedCharacters => combineCharsToFilms(charCount, flatten(resolvedCharacters), films));
+}
+
+function combineCharsToFilms(charCount, allCharacters, films) {
+  return map(films, (film, filmIndex) => {
+    let characterUrls = film.characters.slice(0, charCount);
+
+    const charList = map(characterUrls, (characterUrl, charIndex) => {
+      return find(allCharacters, char => char.url === characterUrl)
+    });
+
+    film.characters = charList;
+    return film;
+  });
 }
