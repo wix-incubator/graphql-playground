796aca875cb3f7446a2bc89905aa05b13445805f
Martynas Bardauskas <martynasb@wix.com>
2016-11-20 23:33:28 +0200

    handling both graph and api calls on client side

--- mobileApp/src/screens/IndexScreen.js
+++ mobileApp/src/screens/IndexScreen.js
@@ -10,6 +10,7 @@ import {
   Text,
   View,
   ScrollView,
+  TouchableHighlight,
   ActivityIndicator
 } from 'react-native';
 
@@ -24,28 +25,60 @@ export default class IndexScreen extends Component {
     }
   }
 
-  componentDidMount() {
-    this.getData()
+  getGraphData() {
+    this.resetData();
+    const query = `
+      query AllFilms($characterCount: Int!) {
+        allFilms{
+          title,
+          description,
+          director,
+          characters(first: $characterCount) {
+            name
+          }
+        }
+      }`;
+
+    const graphQuery = {
+      query,
+      variables: {
+        characterCount: 3
+      }
+    }
+
+    DataService.getGraphData(JSON.stringify(graphQuery))
+      .then(result => this.setGraphData(result))
+      .catch(error => this.handleError(error))
+    ;
   }
 
-  getData() {
-    this.setState({
-      isLoading: true,
-    });
-
-    DataService.getGraphData("{allFilms{title}}")
-      .then(result => this.setNewData(result))
-      .catch(error => this.handleApiError(error))
+  getApiData() {
+    this.resetData();
+    DataService.getApiData()
+      .then(result => this.setApiData(result))
+      .catch(error => this.handleError(error))
+    ;
   }
 
-  setNewData(result) {
+  resetData() {
+    this.setState({films: []});
+  }
+
+  setGraphData(result) {
     this.setState({
       films: result.data.allFilms,
       isLoading: false,
     })
   }
 
-  handleApiError(error) {
+  setApiData(result) {
+    this.setState({
+      films: result,
+      isLoading: false
+    });
+  }
+
+  handleError(error) {
     console.error(error);
     this.setState({
       isLoading: false,
@@ -56,12 +89,7 @@ export default class IndexScreen extends Component {
     return (
       <View style={styles.container}>
         <ScrollView style={{flex: 1}}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit src/screens/IndexScreen.js
-          </Text>
+          {this.renderButtons()}
           <View style={styles.films}>
             <ActivityIndicator animating={this.state.isLoading} size="large" />
             {this.state.films.map((film, index) => this.renderFilm(film, index))}
@@ -72,9 +100,30 @@ export default class IndexScreen extends Component {
   }
 
   renderFilm(film, index) {
+    const characterNames = film.characters.map(char => char.name);
     return (
-      <View key={index}>
-        <Text style={styles.instructions}>{film.title}</Text>
+      <View style={styles.film} key={index}>
+        <View><Text style={styles.filmTitle}>{film.title}</Text></View>
+        <View><Text style={styles.filmDesc}>{film.title}</Text></View>
+        <View>
+          <Text style={styles.filmCharacters}>
+            3 Characters: {characterNames.join(', ')}
+          </Text>
+        </View>
+      </View>
+    )
+  }
+
+  renderButtons() {
+    return (
+      <View style={styles.buttonContainer}>
+        <TouchableHighlight style={styles.button} onPress={this.getGraphData.bind(this)}>
+          <Text>Get data from GraphQL</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.button} onPress={this.getApiData.bind(this)}>
+          <Text>Get data from API</Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -88,17 +137,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     marginTop: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   films: {
     marginTop: 20,
-  }
+  },
+  filmTitle: {
+    fontSize: 21,
+  },
+  filmDesc: {
+    fontSize: 12
+  },
+  filmCharacters: {
+    fontSize: 10,
+    fontStyle: 'italic',
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#ccc',
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10,
+  },
 });
--- mobileApp/src/services/dataService.js
+++ mobileApp/src/services/dataService.js
@@ -1,16 +1,22 @@
 export default {
-  getGraphData
+  getGraphData,
+  getApiData
 }
 
-const BASE_URL = 'http://localhost:5000/graphql';
+const BASE_URL = 'http://localhost:5000';
 
 function getGraphData(graphString) {
-  return fetch(BASE_URL, {
+  return fetch(`${BASE_URL}/graphql`, {
     method: "POST",
     headers: {
-      'Content-Type': "application/graphql"
+      'Content-Type': "application/json"
     },
     body: graphString,
   })
+    .then(response => response.json())
+}
+
+function getApiData() {
+  return fetch(`${BASE_URL}/api/allFilms`)
     .then(response => response.json())
 }
